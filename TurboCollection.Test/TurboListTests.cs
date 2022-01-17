789cbbc1643d42424f9c93aa8ee04a987c68c3e4
Sokratis Papageorgiou <hello@sokpapageorgiou.com>
2022-01-17 15:45:24 +0100

    Add List.Add and Clear

--- TurboCollection.Test/TurboListTests.cs
+++ TurboCollection.Test/TurboListTests.cs
@@ -14,11 +14,47 @@ namespace TurboCollection.Test
         }
 
         [Test]
-        public void AddignAnElementIncreasesCountToOne()
+        public void AddingAnElementIncreasesCountToOne()
         {
             var list = new TList<float>();
             list.Add(5);
             Assert.AreEqual(1, list.Count);
+        }
+
+        [Test]
+        public void AddingAnElementInTheLastPosition()
+        {
+            var list = new TList<float>();
+            list.Add(84);
+            Assert.AreEqual(84, list.List[^1]);
+        }
+
+        [Test]
+        public void GetNumberFromIndex()
+        {
+            var temp = new TList<float>();
+            
+            temp.Add(0);
+            temp.Add(1);
+            temp.Add(2);
+            
+            Assert.AreEqual(0, temp.Get(0));
+            Assert.AreEqual(1, temp.Get(1));
+            Assert.AreEqual(2, temp.Get(2));
+        }
+
+        [Test]
+        public void ClearList()
+        {
+            var temp = new TList<float>();
+            
+            temp.Add(0);
+            temp.Add(1);
+            temp.Add(2);
+
+            temp.Clear();
+            
+            Assert.AreEqual(0, temp.Count);
         }
     }
 }
--- TurboCollections/TList.cs
+++ TurboCollections/TList.cs
@@ -1,15 +1,28 @@
 ﻿using System;
+using System.Linq.Expressions;
 
 namespace TurboCollections
 {
     public class TList<T>
     {
-        public TList()
+        public TList() =>  Console.WriteLine("Hello Turbo!");   
+        
+        private T[] _list = Array.Empty<T>();
+
+        public T[] List => _list;
+
+        public int Count => _list.Length;
+
+        public void Add(T item)
         {
-            Console.WriteLine("Hello Turbo!");            
+            Array.Resize(ref _list, _list.Length + 1);
+            
+            _list[^1] = item;
         }
 
-        public int Count => 0;
+        public T Get(int index) => _list[index];
+
+        public void Clear() => _list = Array.Empty<T>();
     }    
 }
 
