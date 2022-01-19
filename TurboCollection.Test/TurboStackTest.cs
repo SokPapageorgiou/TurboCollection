6a7a40de8ab62f82375ff1904b45233b577e5172
Sokratis Papageorgiou <hello@sokpapageorgiou.com>
2022-01-19 15:11:44 +0100

    Add Push, Peak and Pop

--- TurboCollection.Test/TurboStackTest.cs
+++ TurboCollection.Test/TurboStackTest.cs
@@ -38,5 +38,88 @@ namespace TurboCollection.Test
                 Assert.AreEqual(20, stack.Items.Length);
             }
         }
+
+        public class Count
+        {
+            [Test]
+            public void InitiateCountZero()
+            {
+                var stack = new TStack<float>();
+                
+                Assert.Zero(stack.Count);
+            }
+        }
+
+        public class Push
+        {
+            [Test]
+            public void AddOneItem()
+            {
+                var stack = new TStack<float>();
+
+                stack.Push(10);
+                
+                Assert.AreEqual(10, stack.Items[0]);
+                Assert.AreEqual(1, stack.Count);
+            }
+
+            [Test]
+            public void AddFiveItens()
+            {
+                var stack = new TStack<float>();
+
+                stack.Push(666);
+                stack.Push(555);
+                stack.Push(444);
+                stack.Push(333);
+                stack.Push(222);
+                
+                Assert.AreEqual(666, stack.Items[0]);
+                Assert.AreEqual(555, stack.Items[1]);
+                Assert.AreEqual(444, stack.Items[2]);
+                Assert.AreEqual(333, stack.Items[3]);
+                Assert.AreEqual(222, stack.Items[4]);
+                Assert.AreEqual(5, stack.Count);
+            }
+        }
+
+        public class Peak
+        {
+            [Test]
+            public void PeakLast()
+            {
+                var stack = new TStack<float>();
+
+                stack.Push(666);
+                stack.Push(555);
+                stack.Push(444);
+                stack.Push(333);
+                stack.Push(222);
+                
+                Assert.AreEqual(222, stack.Peak());
+            }
+        }
+
+        public class Pop
+        {
+            [Test]
+            public void GetLastItemAndRemoveFromStack()
+            {
+                var stack = new TStack<float>();
+
+                stack.Push(666);
+                stack.Push(555);
+                stack.Push(444);
+                stack.Push(333);
+                stack.Push(222);
+
+                var temp = stack.Pop();
+                
+                Assert.AreEqual(222, temp);
+                Assert.AreEqual(4, stack.Count);
+            }
+        }
+        
+        
     }
 }
--- TurboCollections/TStack.cs
+++ TurboCollections/TStack.cs
@@ -10,7 +10,7 @@ namespace TurboCollections
 
         public T[] Items => _items;
 
-        public int Count { get; set; }
+        public int Count { get; private set; }
 
         public void IncreaseBuffer()
         {
@@ -23,5 +23,21 @@ namespace TurboCollections
 
             _items = temp;
         }
+
+        public void Push(T value)
+        {
+            Count++;
+            
+            if(Count > _items.Length) IncreaseBuffer();
+
+            _items[Count - 1] = value;
+        }
+
+        public T Peak() => _items[Count - 1];
+
+        public T Pop() => _items[--Count];
+        
+        
+
     }
 }
