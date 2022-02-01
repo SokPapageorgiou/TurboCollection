a2badbdb3f079a0221614f982b6fdac66e654995
Sokratis Papageorgiou <hello@sokpapageorgiou.com>
2022-02-01 16:08:35 +0100

    Add GetLeftChild to TTree

--- TurboCollection.Test/TurboTreeTest.cs
+++ TurboCollection.Test/TurboTreeTest.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using System;
+using NUnit.Framework;
 using TurboCollections;
 
 namespace TurboCollection.Test
@@ -17,6 +18,19 @@ namespace TurboCollection.Test
             Assert.AreEqual(28, tree.GetValue(2));
             Assert.AreEqual(-9, tree.GetValue(3));
             Assert.AreEqual(0, tree.GetValue(4));
+        }
+
+        [Test]
+        public void ReturnLeftChild()
+        {
+            var tree = new TTree<string>();
+
+            tree.Nodes = new[] {"a", "b", "c", "d", "e", "f", "g", "h"};
+            
+            Assert.AreEqual("b", tree.GetLeftChild(0));
+            Assert.AreEqual("d", tree.GetLeftChild(1));
+            Assert.AreEqual("f", tree.GetLeftChild(2));
+            Assert.AreEqual("h", tree.GetLeftChild(3));
         }
     }
 }
--- TurboCollections/TTree.cs
+++ TurboCollections/TTree.cs
@@ -13,6 +13,14 @@ namespace TurboCollections
             set => _nodes = value;
         }
 
+        public T GetLeftChild(int n)
+        {
+            if (n >= _nodes.Length) 
+                return default; 
+                
+            return _nodes[n * 2 + 1];
+        }
+        
         public T GetValue(int n)
         {
             return _nodes[n];
