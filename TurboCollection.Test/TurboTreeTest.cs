43656c5cb68840cb6d464adb4f60b6e2a84ec04c
Sokratis Papageorgiou <hello@sokpapageorgiou.com>
2022-02-01 15:58:15 +0100

    Add TurboTree

---
+++ TurboCollection.Test/TurboTreeTest.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using TurboCollections;
+
+namespace TurboCollection.Test
+{
+    public class TurboTreeTest
+    {
+        [Test]
+        public void ReturnItensFromTree()
+        {
+            var tree = new TTree<float>();
+
+            tree.Nodes = new float[] { 16, 44, 28, -9, 0 };
+            
+            Assert.AreEqual(16, tree.GetValue(0));
+            Assert.AreEqual(44, tree.GetValue(1));
+            Assert.AreEqual(28, tree.GetValue(2));
+            Assert.AreEqual(-9, tree.GetValue(3));
+            Assert.AreEqual(0, tree.GetValue(4));
+        }
+    }
+}
---
+++ TurboCollections/TTree.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Data;
+
+namespace TurboCollections
+{
+    public class TTree<T>
+    {
+        private T[] _nodes;
+        
+        public T[] Nodes
+        {
+            get => _nodes;
+            set => _nodes = value;
+        }
+
+        public T GetValue(int n)
+        {
+            return _nodes[n];
+        }
+    }
+}
