866266439e8935f021dd37d6d31ab93cddf3a36d
Sokratis Papageorgiou <hello@sokpapageorgiou.com>
2022-02-02 10:32:23 +0100

    Starting Over

--- TurboCollection.Test/TurboTreeTest.cs
+++ TurboCollection.Test/TurboTreeTest.cs
@@ -6,44 +6,7 @@ namespace TurboCollection.Test
 {
     public class TurboTreeTest
     {
-        [Test]
-        public void ReturnItensFromTree()
-        {
-            var tree = new TTree();
-
-            tree.Nodes = new float[] { 16, 44, 28, -9, 0 };
+       
             
-            Assert.AreEqual(16, tree.GetValue(0));
-            Assert.AreEqual(44, tree.GetValue(1));
-            Assert.AreEqual(28, tree.GetValue(2));
-            Assert.AreEqual(-9, tree.GetValue(3));
-            Assert.AreEqual(0, tree.GetValue(4));
-        }
-
-        [Test]
-        public void ReturnLeftChild()
-        {
-            var tree = new TTree();
-
-            tree.Nodes = new float[] {0, 1, 2, 3, 4, 5, 6, 7};
-            
-            Assert.AreEqual(1, tree.GetLeftChild(0));
-            Assert.AreEqual(3, tree.GetLeftChild(1));
-            Assert.AreEqual(5, tree.GetLeftChild(2));
-            Assert.AreEqual(7, tree.GetLeftChild(3));
-        }
-        
-        [Test]
-        public void ReturnRightChild()
-        {
-            var tree = new TTree();
-
-            tree.Nodes = new float[] {0, 1, 2, 3, 4, 5, 6, 7};
-            
-            Assert.AreEqual(2, tree.GetRightChild(0));
-            Assert.AreEqual(4, tree.GetRightChild(1));
-            Assert.AreEqual(6, tree.GetRightChild(2));
-            Assert.AreEqual(float.NegativeInfinity, tree.GetRightChild(3));
-        }
     }
 }
--- TurboCollections/TTree.cs
+++ TurboCollections/TTree.cs
@@ -6,46 +6,8 @@ namespace TurboCollections
 {
     public class TTree 
     {
-        private float[] _nodes;
+       
         
-        public float[] Nodes
-        {
-            get => _nodes;
-            set => _nodes = value;
-        }
-
-        public float GetLeftChild(int n)
-        {
-            int index = n * 2 + 1;
-            
-            if (index >= _nodes.Length) 
-                return default!; 
-                
-            return _nodes[index];
-        }
-        
-        public float GetRightChild(int n)
-        {
-            int index = n * 2 + 2;
-            
-            if (index >= _nodes.Length) 
-                return float.NegativeInfinity; 
-                
-            return _nodes[index];
-        }
-        
-        public float GetValue(int n)
-        {
-            return _nodes[n];
-        }
-
-        // public float BinarySearch(float item)
-        // {
-        //     int n = 0;
-        //
-        //     if (item == _nodes[n]) return item;
-        //     if(item(_nodes[n])){}
-        // }
-
     }
 }
+
