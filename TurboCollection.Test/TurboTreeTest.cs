45a943a0e956e3de538d980226ed0664a5d3f27a
Sokratis Papageorgiou <hello@sokpapageorgiou.com>
2022-02-02 16:45:13 +0100

    Add nodes to array

--- TurboCollection.Test/TurboTreeTest.cs
+++ TurboCollection.Test/TurboTreeTest.cs
@@ -6,6 +6,54 @@ namespace TurboCollection.Test
 {
     public class TurboTreeTest
     {
-       
+        [Test]
+        public void AddOneNode()
+        {
+            var tree = new TTree();
+            
+            tree.Add(5);
+            
+            Assert.AreEqual(5, tree.nodes[0].Value);
+        }
+        
+        [Test]
+        public void AddTwoNodeSecondSmaller()
+        {
+            var tree = new TTree();
+            
+            tree.Add(5);
+            tree.Add(2);
+            
+            Assert.AreEqual(2, tree.nodes[1].Value);
+        }
+        
+        [Test]
+        public void AddTwoNodeSecondBigger()
+        {
+            var tree = new TTree();
+            
+            tree.Add(5);
+            tree.Add(10);
+            
+            Assert.AreEqual(10, tree.nodes[2].Value);
+        }
+        
+        [Test]
+        public void AddFiveNodes()
+        {
+            var tree = new TTree();
+            
+            tree.Add(5);
+            tree.Add(10);
+            tree.Add(2);
+            tree.Add(77);
+            tree.Add(-1);
+            
+            Assert.AreEqual(5, tree.nodes[0].Value);
+            Assert.AreEqual(2, tree.nodes[1].Value);
+            Assert.AreEqual(10, tree.nodes[2].Value);
+            Assert.AreEqual(-1, tree.nodes[3].Value);
+            Assert.AreEqual(77, tree.nodes[6].Value);
+        }
     }
 }
--- TurboCollections/TTree.cs
+++ TurboCollections/TTree.cs
@@ -4,9 +4,44 @@ using System.Data;
 
 namespace TurboCollections
 {
+
+    public class TNode
+    {
+        public int Value { get; set; }
+
+        public TNode(int value)
+        {
+            Value = value;
+        }
+    }
+    
     public class TTree
     {
+        public TNode[] nodes = new TNode[12];
+
+        private int _index;
         
+        public void Add(int item)
+        {
+            if (nodes[_index] == null)
+            {
+                nodes[_index] = new TNode(item);
+                _index = 0;
+            }
+            else
+            {
+                if (nodes[_index].Value > item)
+                {
+                    _index = _index * 2 + 1;
+                    Add(item);
+                }
+                else
+                {
+                    _index = _index * 2 + 2;
+                    Add(item);
+                } 
+            }
+        }
     }
 }
 
